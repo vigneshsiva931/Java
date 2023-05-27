526844e56c0209f88991b6acb7da36fafc4544c1
vigneshsiva931 <vigneshsiva437@gmail.com>
2023-05-27 12:50:59 +0530

    Java Sample

---
+++ src/org/java/Vignesh.java
@@ -0,0 +1,13 @@
+package org.java;
+
+public class Vignesh {
+	private void sysou() {
+		System.out.println("Program Starts");
+
+	}
+	public static void main(String[] args) {
+		Vignesh v=new Vignesh();
+		v.sysou();
+	}
+
+}
